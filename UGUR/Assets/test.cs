237cb2120514f887fbb5bd66a99c9342e55da9de
Simonftr <simon.fetre@etu.univ-smb.fr>
2024-06-12 13:52:40 +0200

    dzdq

---
+++ UGUR/Assets/test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit)){
+        }
+        transform.position= raycastHit.point;  
+    }
+}
